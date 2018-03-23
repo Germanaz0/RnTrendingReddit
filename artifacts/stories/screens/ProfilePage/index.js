48105eaf28f9134b15b31ce44e224eb3e2c75857
German Bortoli <germanazo@gmail.com>
2018-03-23 14:26:37 -0300

    Added image slider

--- artifacts/stories/screens/ProfilePage/index.js
+++ artifacts/stories/screens/ProfilePage/index.js
@@ -1,10 +1,29 @@
-import * as React from "react";
-import { Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body } from "native-base";
+import React, { Component } from "react";
+import { Image } from "react-native";
+import { Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body, View } from "native-base";
 import moment from "moment";
 import styles from "./styles";
-class ProfilePage extends React.Component {
+import ImageCarousel from 'react-native-image-carousel';
+class ProfilePage extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = { images: [] };
+    }
+    componentDidMount() {
+        let param = this.props.navigation.state.params;
+        let { preview } = param.data;
+        if (preview) {
+            let images = preview.images.map((im) => {
+                return im.source.url;
+            });
+            this.setState({ images });
+        }
+    }
+    _renderContent(idx) {
+        return (React.createElement(Image, { style: { flex: 1, justifyContent: "center" }, source: { uri: this.state.images[idx] }, resizeMode: "contain" }));
+    }
     render() {
-        const param = this.props.navigation.state.params;
+        let param = this.props.navigation.state.params;
         let { author, title, created_utc } = param.data;
         return (React.createElement(Container, { style: styles.container },
             React.createElement(Header, null,
@@ -15,6 +34,10 @@ class ProfilePage extends React.Component {
                     React.createElement(Title, null, author)),
                 React.createElement(Right, null)),
             React.createElement(Content, { padder: true },
+                React.createElement(View, { style: { flex: 1, justifyContent: "center", alignItems: "center" } },
+                    React.createElement(ImageCarousel, { ref: (imageCarousel) => {
+                            this._imageCarousel = imageCarousel;
+                        }, renderContent: (idx) => this._renderContent(idx) }, this.state.images.map((url) => (React.createElement(Image, { key: url, style: styles.image, source: { uri: url, height: 150, width: 100 }, resizeMode: "contain" }))))),
                 React.createElement(Text, null,
                     React.createElement(Text, { note: true },
                         "[",
--- src/stories/screens/ProfilePage/index.tsx
+++ src/stories/screens/ProfilePage/index.tsx
@@ -1,16 +1,50 @@
-import * as React from "react";
-import {Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body, Footer} from "native-base";
+import React, {Component, ReactElement} from "react";
+import {Image} from "react-native";
+import {Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body, View} from "native-base";
 import moment from "moment";
 import styles from "./styles";
+import ImageCarousel from 'react-native-image-carousel';
 
 export interface Props {
 	navigation: any;
 }
-export interface State {}
-class ProfilePage extends React.Component<Props, State> {
+export interface State {
+	images: any[];
+}
+
+
+class ProfilePage extends Component<Props, State> {
+	state = {images: []};
+	_imageCarousel: ImageCarousel;
+
+	componentDidMount() {
+		let param = this.props.navigation.state.params;
+		let {preview} = param.data;
+
+
+		if (preview) {
+			let images = preview.images.map((im) => {
+				return im.source.url;
+			});
+
+			this.setState({images});
+		}
+
+	}
+	_renderContent(idx: number): ReactElement<any> {
+		return (
+			<Image
+				style={{flex: 1, justifyContent: "center"}}
+				source={{ uri: this.state.images[idx] }}
+				resizeMode={"contain"}
+			/>
+		);
+	}
+
 	render() {
-		const param = this.props.navigation.state.params;
+		let param = this.props.navigation.state.params;
 		let {author, title, created_utc} = param.data;
+
 		return (
 			<Container style={styles.container}>
 				<Header>
@@ -28,9 +62,29 @@ class ProfilePage extends React.Component<Props, State> {
 				</Header>
 
 				<Content padder>
-					<Text>
-						<Text note>[{moment.unix(created_utc).fromNow()}] </Text>
-						{title}</Text>
+
+					<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+						<ImageCarousel
+							ref={(imageCarousel: ImageCarousel) => {
+								this._imageCarousel = imageCarousel;
+							}}
+							renderContent={(idx) => this._renderContent(idx)}
+						>
+							{this.state.images.map((url: string): ReactElement<any> => (
+								<Image
+									key={url}
+									style={styles.image}
+									source={{ uri: url, height: 150, width: 100 }}
+									resizeMode={"contain"}
+								/>
+							))}
+						</ImageCarousel>
+					</View>
+						<Text>
+							<Text note>[{moment.unix(created_utc).fromNow()}] </Text>
+							{title}</Text>
+
+
 				</Content>
 			</Container>
 		);
